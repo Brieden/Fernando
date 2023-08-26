c38f803bea73e3466f181e222b37f8e564670a37
Sven Brieden <sven.brieden@gmail.com>
2023-08-26 16:13:27 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://brieden.github.io",
-  base: '/fernado',
+  base: '/Fernado',
   output: 'static',
   integrations: [
     tailwind({
